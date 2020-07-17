9c9e201bc7e7ce136e552d4808a5a0d6e58e9a15
Chandu <chandushree.kumar@gmail.com>
2020-07-17 23:53:34 +0530

    first commit

---
+++ src/main/java/Maven_ProjectPush/Maven_ProjectPush/App.java
@@ -0,0 +1,13 @@
+package Maven_ProjectPush.Maven_ProjectPush;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
