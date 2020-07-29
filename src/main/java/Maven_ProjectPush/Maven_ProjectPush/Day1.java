371fd3e142f069e2fec35538c109a1e6a30cd667
Chandu <chandushree.kumar@gmail.com>
2020-07-29 13:43:28 +0530

    Day1.java

--- src/main/java/Maven_ProjectPush/Maven_ProjectPush/Day1.java
+++
@@ -1,5 +0,0 @@
-package Maven_ProjectPush.Maven_ProjectPush;
-
-public class Day1 {
-
-}
--- src/main/java/Maven_ProjectPush/Maven_ProjectPush/NewClass.java
+++
@@ -1,7 +0,0 @@
-package Maven_ProjectPush.Maven_ProjectPush;
-
-public class NewClass {
-	public void NewClass1(){
-		System.out.println( "Hello World!!!" );
-	}
-}
